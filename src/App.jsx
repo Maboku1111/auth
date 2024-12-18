ab61456123e2a4ad8beaf51c630ca58a5e453895
Hardy Lutula <hardylutula@gmail.com>
2024-12-18 16:49:33 +0200

    +shadcn.. had to-do abit of tweak since we are using typescript

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import './App.css'
+import {Button} from './components/ui/button'
 
 function App() {
-  
+  return (
+    <div>
+      <Button>Hello there</Button>
+    </div>
+  )
 }
 
 export default App
