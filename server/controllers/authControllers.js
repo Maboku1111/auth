8677270c2098e6b8071b7d204d6b298c3ac0c72f
EzrahCodes <112862267+Maboku1111@users.noreply.github.com>
2024-12-23 20:40:59 +0200

    JWT implemented

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,4 +1,5 @@
-import {prisma} from '../database/dbConfig.js'
+import {prisma} from '../database/dbConfig.js';
+import jwt from 'jsonwebtoken';
 
 export const createUser = async (req, res) => {
   const {username, email, age, password} = req.body
@@ -11,31 +12,37 @@ export const createUser = async (req, res) => {
         password,
       },
     })
-    return res.status(201).json(users)
+    const id = users._id
+
+    const jwt_data = jwt.sign({user_id: id}, 'JWT_SECRET', {expiresIn: '1h'})
+    return res.status(201).json({data: jwt_data})
   } catch (error) {
     console.error(error)
-    return res.status(500).json({ error: 'Internal Server Error' });
+    return res.status(500).json({error: 'Internal Server Error'})
   }
 }
 
 export const getUser = async (req, res) => {
-  const { username, email, password } = req.body;
+  const {username, email, password} = req.body
 
   try {
     const user = await prisma.user.findFirst({
       where: {
-        OR: [{ username: username }, {email: email}],
+        OR: [{username: username}, {email: email}],
         password: password,
       },
-    });
+    })
 
     if (user) {
-      return res.json(user);
+      const id = user._id
+
+      const jwt_data = jwt.sign({user_id: id}, 'JWT_SECRET', {expiresIn: '1h'})
+      return res.json({data: jwt_data})
     } else {
-      return res.status(404).json({ error: 'User not found or incorrect credentials' })
+      return res.json({data: 'try again ! email id not found'})
     }
   } catch (error) {
     console.error(error)
-    return res.status(500).json({ error: 'Internal Server Error' });
+    return res.status(500).json({error: 'Internal Server Error'})
   }
 }
