e0101721918e8ca6becd6009366205cc52c98c43
EzrahCodes <112862267+Maboku1111@users.noreply.github.com>
2024-12-25 08:33:53 +0200

    passwords hashed and login works

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,5 +1,6 @@
-import {prisma} from '../database/dbConfig.js';
-import jwt from 'jsonwebtoken';
+import {prisma} from '../database/dbConfig.js'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 export const createUser = async (req, res) => {
   const {username, email, age, password} = req.body
@@ -29,17 +30,22 @@ export const getUser = async (req, res) => {
     const user = await prisma.user.findFirst({
       where: {
         OR: [{username: username}, {email: email}],
-        password: password,
       },
     })
 
-    if (user) {
-      const id = user._id
+    if (!user) {
+      return res.status(404).json({ data: 'User not found' });
+    }
+
+    const match = bcrypt.compareSync(password, user.password)
+
+    if (match) {
+      const id = user.id
 
       const jwt_data = jwt.sign({user_id: id}, 'JWT_SECRET', {expiresIn: '1h'})
       return res.json({data: jwt_data})
     } else {
-      return res.json({data: 'try again ! email id not found'})
+      return res.json({data: 'try again! user not found'})
     }
   } catch (error) {
     console.error(error)
--- server/database/dbConfig.js
+++ server/database/dbConfig.js
@@ -1,15 +1,27 @@
 /* eslint-disable no-undef */
-import { PrismaClient } from "@prisma/client"
+import {PrismaClient} from '@prisma/client'
+import bcrypt from 'bcrypt'
 
-export const prisma = new PrismaClient()
+export const prisma = new PrismaClient().$extends({
+  query: {
+    user: {
+      $allOperations({operation, args, query}) {
+        if (['create', 'update'].includes(operation) && args.data['password']) {
+          args.data['password'] = bcrypt.hashSync(args.data['password'], 10)
+        }
+        return query(args)
+      },
+    },
+  },
+})
 
 export async function connectToDB() {
   try {
-     await prisma.$connect();
-     console.log("[database]: connected!");
-   } catch (err) {
-     console.log("[database]: connection error: ", err);
-     await prisma.$disconnect();
-     process.exit(1);
-   }
+    await prisma.$connect()
+    console.log('[database]: connected!')
+  } catch (err) {
+    console.log('[database]: connection error: ', err)
+    await prisma.$disconnect()
+    process.exit(1)
+  }
 }
