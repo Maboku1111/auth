27d0ef48d11e9bf703c0e64901cd1ac02d80e4c1
EzrahCodes <112862267+Maboku1111@users.noreply.github.com>
2024-12-28 20:36:13 +0200

    Function to login user along with fetch API

--- service/authService.js
+++ service/authService.js
@@ -3,16 +3,29 @@ const registerUrl = 'http://localhost:3000/auth/register';
 const loginUrl = 'http://localhost:3000/auth/login';
 
 export function registerUser() {
-    fetch(registerUrl)
-        .then((response) => {
-            if (!response.ok) throw new Error('not a valid response')
-            return response.json();
-        })
-        .then((dataobj) => {
-            console.log(dataobj)
-        })
-        .catch((error) => {
-            console.error(error.message)
-        })
+  fetch(registerUrl)
+    .then((response) => {
+      if (!response.ok) throw new Error('not a valid response')
+      return response.json()
+    })
+    .then((dataobj) => {
+      console.log(dataobj)
+    })
+    .catch((error) => {
+      console.error(error.message)
+    })
 }
 
+export function loginUser() {
+  fetch(loginUrl)
+    .then((response) => {
+      if (!response.ok) throw new Error('not a valid response')
+      return response.json()
+    })
+    .then((dataobj) => {
+      console.log(dataobj)
+    })
+    .catch((error) => {
+      console.error(error.message)
+    })
+}
