49b51d50f7ca7f20dae9d49999d52d79ffbf6654
EzrahCodes <112862267+Maboku1111@users.noreply.github.com>
2024-12-28 20:32:05 +0200

    Function to register user along with fetch API

---
+++ service/authService.js
@@ -0,0 +1,18 @@
+// fetch service for authentication
+const registerUrl = 'http://localhost:3000/auth/register';
+const loginUrl = 'http://localhost:3000/auth/login';
+
+export function registerUser() {
+    fetch(registerUrl)
+        .then((response) => {
+            if (!response.ok) throw new Error('not a valid response')
+            return response.json();
+        })
+        .then((dataobj) => {
+            console.log(dataobj)
+        })
+        .catch((error) => {
+            console.error(error.message)
+        })
+}
+
