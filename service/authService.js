1514b050bf2e0c71eb7b278f6830c762dc955e00
EzrahCodes <112862267+Maboku1111@users.noreply.github.com>
2024-12-29 11:10:33 +0200

    Trying to apply DRY principles to my code

--- service/authService.js
+++ service/authService.js
@@ -1,31 +1,18 @@
 // fetch service for authentication
+function fetchData(url) {
+    return fetch(url)
+        .then((response) => {
+            if (!response.ok) throw new Error('not a valid response')
+            return response.json()
+        })
+        .then(data => data)
+        .catch((error) => {
+            console.error(error)
+        })
+}
+
 const registerUrl = 'http://localhost:3000/auth/register';
 const loginUrl = 'http://localhost:3000/auth/login';
 
-export function registerUser() {
-  fetch(registerUrl)
-    .then((response) => {
-      if (!response.ok) throw new Error('not a valid response')
-      return response.json()
-    })
-    .then((dataobj) => {
-      console.log(dataobj)
-    })
-    .catch((error) => {
-      console.error(error.message)
-    })
-}
-
-export function loginUser() {
-  fetch(loginUrl)
-    .then((response) => {
-      if (!response.ok) throw new Error('not a valid response')
-      return response.json()
-    })
-    .then((dataobj) => {
-      console.log(dataobj)
-    })
-    .catch((error) => {
-      console.error(error.message)
-    })
-}
+export const registerUser = fetchData(registerUrl).then(data => console.log(data))
+export const loginUser = fetchData(loginUrl).then(data => console.log(data))
